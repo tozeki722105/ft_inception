2dcac9122e4c1adc2c9bf877e7b6607daf0e656a
tozeki <toshihiro.722105@gmail.com>
2025-03-25 00:22:21 +0900

    fix: phpcon/entrypoint.shの条件文

--- ex03/php/entrypoint.sh
+++ ex03/php/entrypoint.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-if [[ ! -e /var/www/html/wordpress ]]; then
+if [[ ! -e /var/www/html/wordpress/wp-config.php ]]; then
 	cd /var/www/html/wordpress/
 	wp config create --dbname=wordpress --dbuser=test --dbpass=test --dbhost=dbcon --dbcharset=utf8mb4 --allow-root	
+	wp core install --url=ft.42.fr --title=inception --admin_user=master --admin_password=pass --admin_email=info@example.com --path=/var/www/html/wordpress --allow-root
 fi
 
 php-fpm7.4 -F
