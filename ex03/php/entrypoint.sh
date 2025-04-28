0f3cc013c4221f6fb7fcfdc0dd185ff208b9c978
tozeki <toshihiro.722105@gmail.com>
2025-04-29 02:17:21 +0900

    fix: envで大事な情報を管理

--- ex03/php/entrypoint.sh
+++ ex03/php/entrypoint.sh
@@ -2,8 +2,8 @@
 
 if [[ ! -e /var/www/html/wordpress/wp-config.php ]]; then
 	cd /var/www/html/wordpress/
-	wp config create --dbname=wordpress --dbuser=test --dbpass=test --dbhost=dbcon --dbcharset=utf8mb4 --allow-root	
-	wp core install --url=ft.42.fr --title=inception --admin_user=tozeki --admin_password=pass --admin_email=tozeki@example.com --skip-email --path=/var/www/html/wordpress --allow-root
-	wp user create guest guest@example.com --role=editor --user_pass=guestpass --display_name=guest --path=/var/www/html/wordpress --allow-root
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_PASS} --dbhost=dbcon --dbcharset=utf8mb4 --allow-root
+	wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --skip-email --path=/var/www/html/wordpress --allow-root
+	wp user create ${WP_USER_LOGINNAM} ${WP_USER_EMAIL} --role=${WP_USER_ROLE} --user_pass=${WP_USER_PASS} --display_name=${WP_USER_DISPLAYNAME} --path=/var/www/html/wordpress --allow-root
 fi
 php-fpm7.4 -F
