d68ea4454024742dde6d9089a3134a6cf0b37015
tozeki <toshihiro.722105@gmail.com>
2025-04-30 03:12:00 +0900

    fix: rsaに戻した

--- ex03/nginx/entrypoint.sh
+++ ex03/nginx/entrypoint.sh
@@ -5,7 +5,7 @@ SSL_DIR="/etc/nginx/ssl"
 sleep 15
 
 mkdir ${SSL_DIR} && \
-openssl genpkey -algorithm ED25519 -out "${SSL_DIR}/server.key" && \
+openssl genrsa -out "${SSL_DIR}/server.key" 2048 && \
 openssl req -new -key ${SSL_DIR}/server.key -out ${SSL_DIR}/server.csr \
 	-subj "/C=${SITE_COUNTRY}/ST=${SITE_STATE_OR_PROVINCE}/L=${SITE_LOCALITY}/O=${SITE_ORGANIZATION}/CN=${DOMAIN_NAME}" && \
 openssl x509 -req -in ${SSL_DIR}/server.csr -signkey ${SSL_DIR}/server.key \
