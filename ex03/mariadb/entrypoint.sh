8a3562e221c888a5c14a191ca27f9c95e99470f1
tozeki <toshihiro.722105@gmail.com>
2025-04-29 17:48:34 +0900

    fix: passwordをdocker-secretsに移動

--- ex03/mariadb/entrypoint.sh
+++ ex03/mariadb/entrypoint.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+DB_ROOT_PASS=$(cat /run/secrets/db_root_pass)
+DB_USER_PASS=$(cat /run/secrets/db_user_pass)
+
 service mariadb start
 
 mariadb -e "show databases" 2>/dev/null  | grep ${DB_NAME} 1>/dev/null
 if [[! $? -eq 1]]; then
 	mariadb -e "create database ${DB_NAME}" && \
-	mariadb -e "create user '${DB_USER_NAME}'@'%' identified by '${DB_PASS}'" && \
+	mariadb -e "create user '${DB_USER_NAME}'@'%' identified by '${DB_USER_PASS}'" && \
 	mariadb -e "grant all on ${DB_NAME}.* to '${DB_USER_NAME}'@'%'" && \
 	mariadb -e "show grants for '${DB_USER_NAME}'@'%'" && \
 	mariadb -e "flush privileges"
