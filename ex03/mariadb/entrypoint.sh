d9f68d795ef3ff4c36328d7f01efa0cf51736014
tozeki <toshihiro.722105@gmail.com>
2025-05-01 19:53:35 +0900

    fix: rootのpass設定を削除、logの設定を追加

--- ex03/mariadb/entrypoint.sh
+++ ex03/mariadb/entrypoint.sh
@@ -5,21 +5,21 @@ DB_USER_PASS=$(cat /run/secrets/db_user_pass)
 
 service mariadb start
 
-mariadb -e "alter user 'root'@'localhost' identified by '${DB_ROOT_PASS}';" 2>/dev/null \
-	|| echo "The root password is already set"
+sleep 5
 
-mariadb -u root -p"${DB_ROOT_PASS}" -e "show databases" | grep ${DB_NAME} 1>/dev/null
-if [[ $? -eq 1 ]]; then
-	echo "Create a database for wordpress"
-	mariadb -u root -p"${DB_ROOT_PASS}" -e "create database ${DB_NAME}" && \
-	mariadb -u root -p"${DB_ROOT_PASS}" -e "create user '${DB_USER_NAME}'@'%' identified by '${DB_USER_PASS}'" && \
-	mariadb -u root -p"${DB_ROOT_PASS}" -e "grant all on ${DB_NAME}.* to '${DB_USER_NAME}'@'%'" && \
-	mariadb -u root -p"${DB_ROOT_PASS}" -e "show grants for '${DB_USER_NAME}'@'%'" && \
-	mariadb -u root -p"${DB_ROOT_PASS}" -e "flush privileges"
-	echo "Finish for creating a database for wordpress"
+mariadb -e "show databases" 2>/dev/null | grep ${DB_NAME} 1>/dev/null
+if (($? == 1)); then
+	echo "Create a database for WordPress..."
+	mariadb -e "create database ${DB_NAME}" && \
+	mariadb -e "create user '${DB_USER_NAME}'@'%' identified by '${DB_USER_PASS}'" && \
+	mariadb -e "grant all on ${DB_NAME}.* to '${DB_USER_NAME}'@'%'" && \
+	mariadb -e "show grants for '${DB_USER_NAME}'@'%'" && \
+	mariadb -e "flush privileges" && \
+	echo "Finish for creating a database for WordPress"
+else
+	echo "Already exit a database for WordPress"
 fi
 
 service mariadb stop
 
 mysqld_safe
-
