a6770eb2c3d3f68de671f295a71f6f3a40414166
tozeki <toshihiro.722105@gmail.com>
2025-05-02 04:14:12 +0900

    del: 余計なディレクトリ

--- test_env/mariadb/init.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-service mariadb start && \
-mariadb -e "create database ${DB_NAME}" && \
-mariadb -e "create user '${DB_USER_NAME}'@'%' identified by '${DB_PASS}'" && \
-mariadb -e "grant all on ${DB_NAME}.* to '${DB_USER_NAME}'@'%'" && \
-mariadb -e "show grants for '${DB_USER_NAME}'@'%'" && \
-mariadb -e "flush privileges" && \
-service mariadb stop
