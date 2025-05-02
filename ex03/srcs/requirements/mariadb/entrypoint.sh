4fdce31d1038e9c9b24853a8f9d9af2422b3bcec
tozeki <toshihiro.722105@gmail.com>
2025-05-02 14:32:44 +0900

    clean: 出力分の修正と,不要なパッケージを排除

--- ex03/srcs/requirements/mariadb/entrypoint.sh
+++ ex03/srcs/requirements/mariadb/entrypoint.sh
@@ -6,17 +6,17 @@ service mariadb start
 
 sleep 5
 
-mariadb -e "show databases" 2>/dev/null | grep ${DB_NAME} 1>/dev/null
+mariadb -e "SHOW DATABASES" 2>/dev/null | grep ${DB_NAME} 1>/dev/null
 if (($? == 1)); then
-	echo "Create a database for WordPress..."
-	mariadb -e "create database ${DB_NAME}" && \
-	mariadb -e "create user '${DB_USER_NAME}'@'%' identified by '${DB_USER_PASS}'" && \
-	mariadb -e "grant all on ${DB_NAME}.* to '${DB_USER_NAME}'@'%'" && \
-	mariadb -e "show grants for '${DB_USER_NAME}'@'%'" && \
-	mariadb -e "flush privileges" && \
-	echo "Finish for creating a database for WordPress"
+	echo "Creating WordPress database and user..."
+	mariadb -e "CREATE DATABASE ${DB_NAME}" && \
+	mariadb -e "CREATE USER '${DB_USER_NAME}'@'%' IDENTIFIED BY '${DB_USER_PASS}'" && \
+	mariadb -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER_NAME}'@'%'" && \
+	mariadb -e "SHOW GRANTS FOR '${DB_USER_NAME}'@'%'" && \
+	mariadb -e "FLUSH PRIVILEGES" && \
+	echo "WordPress database and user created successfully."
 else
-	echo "Already exit a database for WordPress"
+	echo "WordPress database already exists."
 fi
 
 service mariadb stop
--- ex03/srcs/requirements/wordpress/entrypoint.sh
+++ ex03/srcs/requirements/wordpress/entrypoint.sh
@@ -4,28 +4,30 @@ DB_USER_PASS=$(cat /run/secrets/db_user_pass)
 WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_pass)
 WP_USER_PASS=$(cat /run/secrets/wp_user_pass)
 
+echo "Waiting for MariaDB database to be ready..."
 limit=8
 i=0
 while ! mysqladmin ping -h dbcon -u ${DB_USER_NAME} -p${DB_USER_PASS} 2>/dev/null | grep "mysqld is alive" 2>/dev/null ; do
 	if (($i == $limit)); then
-		echo "Error: mysqladmin ping"
+		echo "Error: Failed to connect to MariaDB"
 		exit 2
 	fi
 	((i++))
 	sleep 2;
 done
+echo "MariaDB database connection established successfully."
 
 if [[ ! -f /var/www/html/wordpress/wp-config.php ]]; then
-	echo "Install Wordpress..."
+	echo "Initializing WordPress installation..."
 	cd /var/www/html/wordpress/
 	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_USER_PASS} --dbhost=dbcon --dbcharset=utf8mb4 --allow-root && \
 	wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASS} \
 		--admin_email=${WP_ADMIN_EMAIL} --skip-email --path=/var/www/html/wordpress --allow-root && \
 	wp user create ${WP_USER_LOGINNAME} ${WP_USER_EMAIL} --role=${WP_USER_ROLE} --user_pass=${WP_USER_PASS} \
 		--display_name=${WP_USER_DISPLAYNAME} --path=/var/www/html/wordpress --allow-root && \
-	echo "Finish installation"
+	echo "WordPress installation completed successfully."
 else
-	echo "Already exit WordPress"
+	echo "WordPress is already configured."
 fi
 
 php-fpm7.4 -F
